d0a5fbfbcdc894fbe88917af66a74ca92a66f42e
Kirra Hyde <kirra.hyde@gmail.com>
2024-03-16 01:28:08 -0400

    story tests

--- js/models.test.js
+++ js/models.test.js
@@ -1,38 +1,32 @@
-describe('Story', function() {
-  it('should create a Story instance with the correct properties', function() {
-    let storyData = {
-      storyId: 1,
-      title: 'Test Story',
-      author: 'Test Author',
-      url: 'https://www.example.com',
-      username: 'testuser',
-      createdAt: new Date()
-    };
-    let story = new Story(storyData);
+"use strict";
 
-    expect(story.storyId).toEqual(1);
-    expect(story.title).toEqual('Test Story');
-    // etc
+const mock = new AxiosMockAdapter(axios);
+
+const storyData = {
+  storyId: "123",
+  title: "Test Story",
+  author: "Test Author",
+  url: "https://www.example.com/path",
+  username: "testuser",
+  createdAt: "2024-03-16T00:00:12.142Z"
+}
+
+describe("Story", function () {
+
+  it("getHostName", function () {
+    const story = new Story(storyData);
+    expect(story.getHostName()).toEqual("www.example.com");
   });
 
-  it('should correctly parse the hostname from the URL', function() {
-    let story = new Story({ url: 'https://www.testdomain.com/path' });
-    expect(story.getHostName()).toEqual('www.testdomain.com');
-  });
+  // getStory uses axios, so testing it requires mocking axios
+  it("getStory", async function () {
+    // Mock axios.get to return predefined story data
+    mock.onGet(`${BASE_URL}/stories/123`).reply(200, {
+      story: storyData,
+    });
 
-  //Testing the static method requires mocking axios
-  it('should fetch a story from the API', async function() {
-    // Mock axios.get to return a predefined story object
-    const mockStory = {
-        storyId: 1,
-        title: 'Fetched Story',
-        author: 'Test Author',
-        url: 'https://www.example.com',
-        username: 'testuser',
-        createdAt: new Date()
-      };
-    axios.get = jasmine.createSpy('axios.get').and.returnValue(Promise.resolve({ data: { story: mockStory } }));
-    let story = await Story.getStory(1)
-    expect(story.title).toEqual('Fetched Story');
+    const story = await Story.getStory("123");
+    expect(story).toBeInstanceOf(Story);
+    expect(story.title).toEqual("Test Story");
   });
 });
---
+++ js/start.js
@@ -0,0 +1,2 @@
+//Once the DOM is loaded, begin the app
+$(start);
